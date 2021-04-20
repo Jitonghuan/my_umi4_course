7d86f886ee1ddc27feb549fe111e35d70d863c68
afang.yyf <yaoyongfang@come-future.com>
2021-04-20 16:25:05 +0800

    refactor: 补充测试环境

---
+++ config/config.test.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  publicPath:
+    'https://come2future-web.oss-cn-hangzhou.aliyuncs.com/dev/fe-matrix-front/matrix-front/',
+});
