ad1f050aa9c4cd338683d86de7c73c1b98e4ed9a
moyan <moyan@come-future.com>
2021-05-30 11:57:13 +0800

    feat: 完成函数管理开发及联调

--- src/pages/test/functions/index.tsx
+++ src/pages/test/functions/index.tsx
@@ -2,15 +2,191 @@
 // @author CAIHUAZHI <moyan@come-future.com>
 // @create 2021/05/30 10:10
 
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+  useRef,
+} from 'react';
+import { Form, Input, Table, Button, Modal, message, Popconfirm } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import FELayout from '@cffe/vc-layout';
 import MatrixPageContent from '@/components/matrix-page-content';
 import { ContentCard } from '@/components/vc-page-content';
+import {} from './interfaces';
+import * as APIS from './service';
+import { getRequest, postRequest } from '@/utils/request';
 import './index.less';
 
 export default function FunctionManager() {
+  const userInfo = useContext(FELayout.SSOUserInfoContext);
+  const [keyword, setKeyword] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+  const [pageIndex, setPageIndex] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [dataSource, setDataSource] = useState<Record<string, any>[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const [editVisible, setEditVisible] = useState<boolean>(false);
+  const [editField] = Form.useForm<{ content: string; id?: number }>();
+  const editModelTitle = useRef('新增函数');
+
+  const handleSearch = () => {
+    pageIndex === 1 ? queryData() : setPageIndex(1);
+  };
+
+  const queryData = () => {
+    setLoading(true);
+    getRequest(APIS.funcList, {
+      data: { keyword, pageIndex, pageSize },
+    })
+      .then((result) => {
+        const { data } = result;
+        setDataSource(data || []);
+
+        setTotal(100); // TODO 返回的数据里面没有分页信息!!
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    queryData();
+  }, [pageIndex, pageSize]);
+
+  // 新增函数
+  const handleAddClick = useCallback(() => {
+    editModelTitle.current = '新增函数';
+    editField.resetFields();
+    setEditVisible(true);
+  }, []);
+
+  const handleEditConfirm = useCallback(async () => {
+    const { content, id } = await editField.validateFields();
+
+    if (!id) {
+      // 新增
+      postRequest(APIS.addFunc, {
+        data: {
+          funcBody: content, // TODO 数据怎么加密？
+          createUser: userInfo.userName,
+          modifyUser: userInfo.userName,
+        },
+      }).then(() => {
+        message.success('新增成功！');
+        setKeyword('');
+        setEditVisible(false);
+        pageIndex === 1 ? queryData() : setPageIndex(1);
+      });
+    } else {
+      // 修改
+      postRequest(APIS.updateFunc, {
+        data: {
+          id,
+          funcBody: content,
+          modifyUser: userInfo.userName,
+        },
+      }).then(() => {
+        message.success('修改成功！');
+        setEditVisible(false);
+        queryData();
+      });
+    }
+  }, [editField]);
+
+  const handleModify = useCallback(
+    (record: Record<string, any>, index: number) => {
+      editModelTitle.current = '修改函数';
+      editField.resetFields();
+      editField.setFieldsValue({
+        content: record.func || '',
+        id: record.id,
+      });
+      setEditVisible(true);
+    },
+    [dataSource],
+  );
+
+  const confirmDelItem = useCallback(
+    async (record: Record<string, any>, index: number) => {
+      await postRequest(APIS.delFunc, {
+        data: { id: record.id },
+      });
+      message.success('删除成功！');
+      queryData();
+    },
+    [dataSource],
+  );
+
   return (
     <MatrixPageContent>
-      <ContentCard>hello~~</ContentCard>
+      <ContentCard className="page-test-functions">
+        <div className="test-page-header">
+          <Input.Search
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            onPressEnter={() => handleSearch()}
+            onSearch={() => handleSearch()}
+            style={{ width: 320 }}
+          />
+          <Button type="primary" onClick={handleAddClick}>
+            <PlusOutlined /> 新增函数
+          </Button>
+        </div>
+        <Table
+          dataSource={dataSource}
+          loading={loading}
+          pagination={{
+            current: pageIndex,
+            total,
+            pageSize,
+            showSizeChanger: true,
+            onChange: (next) => setPageIndex(next),
+            onShowSizeChange: (_, next) => setPageSize(next),
+          }}
+        >
+          <Table.Column title="序号" dataIndex="id" />
+          <Table.Column title="函数名" dataIndex="name" />
+          <Table.Column title="描述" dataIndex="desc" />
+          <Table.Column title="创建人" dataIndex="createUser" />
+          <Table.Column title="操作时间" dataIndex="gmtModify" />
+          <Table.Column
+            title="操作"
+            width={120}
+            render={(_, record: Record<string, any>, index) => (
+              <div className="action-cell">
+                <a onClick={(e) => handleModify(record, index)}>修改</a>
+                <Popconfirm
+                  title="确定要删除该函数吗？"
+                  onConfirm={() => confirmDelItem(record, index)}
+                >
+                  <a>删除</a>
+                </Popconfirm>
+              </div>
+            )}
+          />
+        </Table>
+      </ContentCard>
+      <Modal
+        visible={editVisible}
+        title={editModelTitle.current}
+        width={800}
+        onOk={handleEditConfirm}
+        onCancel={() => setEditVisible(false)}
+      >
+        <Form form={editField}>
+          <Form.Item noStyle name="id">
+            <Input type="hidden" />
+          </Form.Item>
+          <Form.Item
+            name="content"
+            rules={[{ required: true, message: '函数内容不能为空！' }]}
+          >
+            <Input.TextArea placeholder="请输入函数" rows={10} />
+          </Form.Item>
+        </Form>
+      </Modal>
     </MatrixPageContent>
   );
 }
