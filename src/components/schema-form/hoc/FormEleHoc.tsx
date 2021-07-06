4aa956cd625f4ac9115854e902aa5383a07a8318
afang.yyf <yaoyongfang@come-future.com>
2021-07-06 10:45:12 +0800

    refactor: 补充组件

---
+++ src/components/schema-form/hoc/FormEleHoc.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { InputProps } from 'antd/es/input';
+
+import { INodeType } from '../type';
+
+const defaultMap = {
+  Input: {
+    allowClear: true,
+    placeholder: '请输入',
+  },
+  InputNumber: {
+    placeholder: '请输入',
+  },
+  Select: {
+    allowClear: true,
+    placeholder: '请选择',
+    showSearch: true,
+    optionFilterProp: 'label',
+  },
+} as { [key: string]: any };
+
+export default (WrappedComponent: React.ComponentClass | any, type: INodeType) => {
+  const defaultProps = defaultMap[type];
+  return (props: InputProps | any) => {
+    const { customMap = {}, ...restProps } = props;
+    const extraProps = type === 'Custom' ? { customMap } : {};
+    return <WrappedComponent {...defaultProps} {...restProps} {...extraProps} />;
+  };
+};
---
+++ src/components/schema-form/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import BasicForm from './theme/BasicForm';
+import InlineForm from './theme/InlineForm';
+import { IProps as IFEForm } from './wrapper';
+import { componentsEnum as totalComponentsEnum } from './hooks/useComponents';
+
+export type ITheme = 'basic' | 'inline';
+
+export interface IProps extends IFEForm {
+  /** 主题 */
+  theme?: ITheme;
+}
+
+export const themeEnum = {
+  basic: BasicForm,
+  inline: InlineForm,
+} as { [key: string]: any };
+
+// 枚举所有注册的可用组件
+export const componentsEnum = [...totalComponentsEnum];
+
+/**
+ * form 表单容器
+ * @create 2021-01-06 12:45
+ */
+const FEFormWrapper = (props: IProps) => {
+  const { theme = 'basic', ...rest } = props;
+  const TargetForm = themeEnum[theme];
+
+  return <TargetForm {...rest} />;
+};
+
+/**
+ * 默认值
+ */
+FEFormWrapper.defaultProps = {
+  // 属性默认值配置
+  theme: 'basic',
+};
+
+export default FEFormWrapper;
