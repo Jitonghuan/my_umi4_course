b3884658969a1a6f9b7c70113387a20c1213c4a7
moyan <moyan@come-future.com>
2021-07-06 20:50:44 +0800

    feat: add empty file

---
+++ src/pages/test/autotest/task-manager/hooks.ts
@@ -0,0 +1,6 @@
+// task manager hooks
+// @author CAIHUAZHI <moyan@come-future.com>
+// @create 2021/07/06 20:15
+
+import * as APIS from '../service';
+import { getRequest, postRequest } from '@/utils/request';
