4144043337cc0387048fa334de88c1cfd9148797
moyan <moyan@come-future.com>
2021-08-03 10:05:41 +0800

    chore: save

---
+++ config/fe.config.js
@@ -0,0 +1,23 @@
+// fe config
+// @author CAIHUAZHI <moyan@come-future.com>
+// @create 2021/08/03 10:02
+
+module.exports = {
+  type: 'builder',
+  builder: '@cffe/fe-builder-default',
+  commands: {
+    // 可以直接是数组，也可以是一个方法，返回一个数组
+    publish: (options) => {
+      const buildEnv = options.online ? 'prod' : 'test';
+      const ossDir = options.online ? 'prod' : 'dev';
+
+      return [
+        '$ fnpm install',
+        `$ npm run build:${buildEnv}`,
+        `#oss -r ./build come2future-web:${ossDir}/{{group}}/{{project}}`,
+        '$ sshpass -p "&WUb&1u8508P0ohD" ssh root@192.168.0.111 "cd /usr/share/nginx/html/matrix-test/matrix && rm -rf index.html && wget https://come2future-web.oss-cn-hangzhou.aliyuncs.com/dev/fe-matrix-front/matrix-front/matrix/index.html"',
+        '$ echo PUBLISH SUCCESS!!',
+      ];
+    },
+  },
+};
