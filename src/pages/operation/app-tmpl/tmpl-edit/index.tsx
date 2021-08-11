4a65e098a833a32d3958ffe563d8560b06532475
muxi.jth <muxi.jth@cfuture-fake.com>
2021-08-11 16:58:32 +0800

    feat:去掉应用模版编辑和复制页的必选项

--- src/pages/operation/app-tmpl/tmpl-edit/index.tsx
+++ src/pages/operation/app-tmpl/tmpl-edit/index.tsx
@@ -122,8 +122,8 @@ export default function DemoPageTb(porps: any) {
   const createTmpl = (value: any) => {
     const templateCode: string = porps.history.location.query.templateCode;
     //  const tmplConfigurableItem = new Map(value.tmplConfigurableItem.map((el:any)=> [el.key,el.value]))
-    const tmplConfigurableItem = value.tmplConfigurableItem.reduce((prev: any, el: any) => {
-      prev[el.key] = el.value;
+    const tmplConfigurableItem = value?.tmplConfigurableItem?.reduce((prev: any, el: any) => {
+      prev[el.key] = el?.value;
       return prev;
     }, {} as any);
     putRequest(APIS.update, {
@@ -131,15 +131,15 @@ export default function DemoPageTb(porps: any) {
         templateName: value.templateName,
         templateType: value.templateType,
         templateValue: value.templateValue,
-        appCategoryCode: value.appCategoryCode,
-        envCode: value.envCodes,
-        tmplConfigurableItem,
+        appCategoryCode: value.appCategoryCode || '',
+        envCode: value.envCodes || [],
+        tmplConfigurableItem: tmplConfigurableItem || {},
         templateCode: templateCode,
       },
     }).then((resp: any) => {
       if (resp.success) {
         const datas = resp.data || [];
-        setEnvDatas(datas);
+        setEnvDatas(datas.envCode);
         history.push({
           pathname: 'tmpl-list',
         });
@@ -174,7 +174,7 @@ export default function DemoPageTb(porps: any) {
 
             <Col span={10} offset={2}>
               <div style={{ fontSize: 18 }}>可配置项：</div>
-              <Form.Item name="tmplConfigurableItem" rules={[{ required: true, message: '这是必填项' }]}>
+              <Form.Item name="tmplConfigurableItem">
                 <EditorTable
                   columns={[
                     { title: 'Key', dataIndex: 'key', colProps: { width: 240 } },
@@ -191,7 +191,6 @@ export default function DemoPageTb(porps: any) {
                 label="选择默认应用分类："
                 labelCol={{ span: 8 }}
                 name="appCategoryCode"
-                rules={[{ required: true }]}
                 style={{ marginTop: '140px' }}
               >
                 <Select
@@ -202,12 +201,7 @@ export default function DemoPageTb(porps: any) {
                   disabled={isDisabled}
                 />
               </Form.Item>
-              <Form.Item
-                label="选择默认环境："
-                labelCol={{ span: 8 }}
-                name="envCodes"
-                rules={[{ required: true, message: '这是必选项' }]}
-              >
+              <Form.Item label="选择默认环境：" labelCol={{ span: 8 }} name="envCodes">
                 <Select
                   mode="multiple"
                   allowClear
