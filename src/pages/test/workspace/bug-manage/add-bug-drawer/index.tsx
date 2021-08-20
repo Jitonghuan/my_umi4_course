9ee115cb090d888797b6e912126e5f56b752dacf
jidan.wd <wudi@come-future.com>
2021-08-20 15:15:51 +0800

    feat: 测试管理-测试工作台-Bug跟踪 新增Bug UI还原

--- src/pages/test/workspace/bug-manage/add-bug-drawer/index.tsx
+++ src/pages/test/workspace/bug-manage/add-bug-drawer/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext, useMemo } from 'react';
-import { Select, Input, Switch, Button, Table, Form, Space, Drawer, Row, Col, Checkbox, message } from 'antd';
 import { bugTypeEnum, statusEnum, priorityEnum } from '../../constant';
+import { Select, Input, Switch, Button, Table, Form, Space, Drawer, Row, Col, Checkbox, message, Radio } from 'antd';
 import { addBug, modifyBug } from '../../service';
 import { createSona } from '@cffe/sona';
 import RichText from '@/components/rich-text';
@@ -11,13 +11,12 @@ import { postRequest } from '@/utils/request';
 export default function BugManage(props: any) {
   const { visible, setVisible, projectList, bugInfo, updateBugList } = props;
   const userInfo = useContext(FELayout.SSOUserInfoContext);
-  const [continueAdd, setContinueAdd] = useState(false);
   const [relatedCases, setRelatedCases] = useState<any[]>([]);
   const [schema, setSchema] = useState<any[]>();
   const [form] = Form.useForm();
   const sona = useMemo(() => createSona(), []);
 
-  const submit = async () => {
+  const submit = async (continueAdd = false) => {
     const finishLoading = message.loading(bugInfo ? '正在修改' : '正在新增');
     const formData = form.getFieldsValue();
     const requestParams = {
@@ -55,103 +54,97 @@ export default function BugManage(props: any) {
     }
   }, [visible]);
 
+  const formItemLayout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 19 },
+  };
+
   return (
     <Drawer
-      visible={visible}
+      visible={true}
       onClose={() => setVisible(false)}
       width={700}
       title={bugInfo ? '编辑Bug' : '新增Bug'}
       className="test-workspace-bug-manage-add-bug-drawer"
       maskClosable={false}
     >
-      <Form labelCol={{ style: { width: '100px' } }} labelAlign="left" form={form}>
-        <Row gutter={[16, 0]}>
-          <Col span="24">
-            <Form.Item label="标题" name="name">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span="12">
-            <Form.Item label="所属业务" name="business">
-              <Select>
-                {projectList.map((item: any) => (
-                  <Select.Option value={item.id} key={item.id}>
-                    {item.categoryName}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span="12">
-            <Form.Item label="优先级" name="priority">
-              <Select>
-                {priorityEnum.map((title, index) => (
-                  <Select.Option value={index} key={index}>
-                    {title}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span="12">
-            <Form.Item label="类型" name="bugType">
-              <Select>
-                {bugTypeEnum.map((title, index) => (
-                  <Select.Option value={index} key={index}>
-                    {title}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span="12">
-            <Form.Item label="是否线上Bug" valuePropName="checked" name="onlineBug">
-              <Switch />
-            </Form.Item>
-          </Col>
-          <Col span="24">
-            <Form.Item label="描述" name="desc">
-              <RichText width="560px" sona={sona} schema={schema} />
-            </Form.Item>
-          </Col>
-          <Col span="24">
-            <Form.Item label="关联用例" name="relatedCases">
-              icon1,icon2,list
-            </Form.Item>
-          </Col>
-          <Col span="12">
-            <Form.Item label="经办人" name="agent">
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span="12" />
-          <Col span="12">
-            <Form.Item label="状态" name="status">
-              <Select>
-                {statusEnum.map((title, index) => (
-                  <Select.Option value={index} key={index}>
-                    {title}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
+      <Form {...formItemLayout} form={form}>
+        <Form.Item label="标题" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="所属业务" name="business">
+          <Select>
+            {projectList.map((item: any) => (
+              <Select.Option value={item.id} key={item.id}>
+                {item.categoryName}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item label="优先级" name="priority">
+          <Radio.Group>
+            {priorityEnum.map((title, index) => (
+              <Radio value={index} key={index}>
+                {title}
+              </Radio>
+            ))}
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="类型" name="bugType">
+          <Select>
+            {bugTypeEnum.map((title, index) => (
+              <Select.Option value={index} key={index}>
+                {title}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item label="是否线上Bug" valuePropName="checked" name="onlineBug">
+          <Switch />
+        </Form.Item>
+        <Form.Item label="关联用例" name="relatedCases">
+          <Space>
+            <Button type="primary" ghost>
+              关联用例
+            </Button>
+            <Button type="primary" ghost>
+              新增用例
+            </Button>
+          </Space>
+        </Form.Item>
+        <Form.Item label="描述" name="desc">
+          <RichText width="524px" sona={sona} schema={schema} />
+        </Form.Item>
+        <Form.Item label="经办人" name="agent">
+          <Input />
+        </Form.Item>
+        <Form.Item label="状态" name="status">
+          <Select>
+            {statusEnum.map((title, index) => (
+              <Select.Option value={index} key={index}>
+                {title}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
       </Form>
+
       <div className="footer">
-        {!bugInfo ? (
-          <Checkbox checked={continueAdd} onChange={(e) => setContinueAdd(e.target.checked)}>
-            继续新增下一个
-          </Checkbox>
-        ) : (
-          ''
-        )}
-        <Button type="primary" className="ml-auto" onClick={() => setVisible(false)}>
-          取消
-        </Button>
-        <Button type="primary" className="ml-12" onClick={submit}>
-          确定
-        </Button>
+        <Space>
+          {!bugInfo ? (
+            <Button type="primary" onClick={() => submit(true)}>
+              保存并新增
+            </Button>
+          ) : (
+            ''
+          )}
+          <Button type="primary" onClick={() => submit(false)}>
+            保存
+          </Button>
+          <Button className="ml-auto" onClick={() => setVisible(false)}>
+            取消
+          </Button>
+        </Space>
       </div>
     </Drawer>
   );
--- src/pages/test/workspace/constant.ts
+++ src/pages/test/workspace/constant.ts
@@ -1,3 +1,3 @@
 export const statusEnum = ['新建', '修复中', '已拒绝', '待验证', '重复打开', '已关闭', '延期解决'];
 export const bugTypeEnum = ['功能问题', '性能问题', '接口问题', 'UI界面问题', '易用性问题', '需求问题'];
-export const priorityEnum = ['P0', 'P1', 'P2', 'P3'];
+export const priorityEnum = ['高', '中', '低'];
