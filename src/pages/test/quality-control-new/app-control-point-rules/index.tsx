808810769229aec198d3450a4d95b3b10f3b50f4
jidan.wd <wudi@come-future.com>
2021-09-13 15:58:48 +0800

    feat: 测试管理-质量控制-规则配置 UI

--- src/pages/test/quality-control-new/app-control-point-rules/index.tsx
+++ src/pages/test/quality-control-new/app-control-point-rules/index.tsx
@@ -2,12 +2,44 @@ import React, { useState, useEffect, useContext } from 'react';
 import { ContentCard } from '@/components/vc-page-content';
 import PageContainer from '@/components/page-container';
 import HeaderTabs from '../_components/header-tabs';
+import { Table, Button, Input, Form, Space, Typography } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import './index.less';
 
 export default function AppControlPointRules(props: any) {
   return (
     <PageContainer className="quality-control-app-control-point-rules">
       <HeaderTabs activeKey="app-control-point-rules" history={props.history} />
-      <ContentCard>page AppControlPointRules</ContentCard>
+      <ContentCard>
+        <div className="header">
+          <div className="title-and-search">
+            <Typography.Text strong>已配置规则列表</Typography.Text>
+            <Input.Search className="keyword-search-input" placeholder="输入服务关键字搜索" />
+          </div>
+          <Button type="primary">
+            <PlusOutlined />
+            新增规则
+          </Button>
+        </div>
+        <Table dataSource={[]}>
+          <Table.Column title="服务" dataIndex="?" />
+          <Table.Column title="创建时间" dataIndex="?" />
+          <Table.Column title="更新时间" dataIndex="?" />
+          <Table.Column
+            title="操作"
+            width="180"
+            render={() => {
+              return (
+                <Space>
+                  <Button type="link">查看</Button>
+                  <Button type="link">编辑</Button>
+                  <Button type="link">删除</Button>
+                </Space>
+              );
+            }}
+          />
+        </Table>
+      </ContentCard>
     </PageContainer>
   );
 }
