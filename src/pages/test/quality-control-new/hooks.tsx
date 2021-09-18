df235a1010c795ea96504a7819ecd2030b9921c9
jidan.wd <wudi@come-future.com>
2021-09-18 11:51:36 +0800

    feat: 测试管理-质量控制-质量看板 使用HOOKS

--- src/pages/test/quality-control-new/hooks.tsx
+++ src/pages/test/quality-control-new/hooks.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { getRequest } from '@/utils/request';
 import * as APIS from './service';
-import type * as ITERFACES from './interface';
+import type * as INTERFACES from './interface';
+import { IConfig } from '_@umijs_core@3.5.18@@umijs/core';
 
 export function useAppCateEnum() {
   const [data, setData] = useState<IOption[]>([]);
@@ -38,7 +39,7 @@ export function useAppCodeEnum() {
 }
 
 export function useAllAppCodeQualityConf(keyword: string) {
-  const [data, setData] = useState<ITERFACES.IConfig[]>([]);
+  const [data, setData] = useState<INTERFACES.IConfig[]>([]);
 
   useEffect(() => {
     getRequest(APIS.getAllAppCodeQualityConf).then((res) => {
@@ -49,6 +50,223 @@ export function useAllAppCodeQualityConf(keyword: string) {
   if (keyword?.length) {
     return [data.filter((item) => item.appCode.includes(keyword) || item.categoryCode.includes(keyword))];
   }
+
+  return [data];
+}
+
+export function useAllRanking() {
+  const [data, setData] = useState<INTERFACES.IRanking>({});
+
+  useEffect(() => {
+    getRequest(APIS.getRanking).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  return [data];
+}
+
+export function useAllAppServices() {
+  const [data, setData] = useState<IConfig[]>([]);
+
+  // useEffect(() => {
+  //   getRequest(APIS.getRanking).then((res) => {
+  //     setData(res.data);
+  //   });
+  // }, []);
+
+  return [data];
+}
+
+export function useAppTrendMap() {
+  const [data, setData] = useState<INTERFACES.ITrend>({
+    qualityPoints: {
+      data: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+      xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    },
+    codeLines: {
+      data: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+      xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    },
+    codeBugs: {
+      data: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+      xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    },
+    utPassRate: {
+      data: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+      xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    },
+    codeDuplicationsRate: {
+      data: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+      xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    },
+    utCovRate: {
+      data: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          data: [120, 132, 101, 134, 90, 230, 210],
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          data: [220, 182, 191, 234, 290, 330, 310],
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          data: [150, 232, 201, 154, 190, 330, 410],
+        },
+        {
+          name: '直接访问',
+          type: 'line',
+          data: [320, 332, 301, 334, 390, 330, 320],
+        },
+        {
+          name: '搜索引擎',
+          type: 'line',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+        },
+      ],
+      xAxis: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    },
+  });
+
+  // useEffect(() => {
+  //   getRequest(APIS.getRanking).then((res) => {
+  //     setData(res.data);
+  //   });
+  // }, []);
 
   return [data];
 }
--- src/pages/test/quality-control-new/interface.ts
+++ src/pages/test/quality-control-new/interface.ts
@@ -5,3 +5,20 @@ export interface IConfig {
   gmtCreate: string;
   gmtModify: string;
 }
+
+export interface IRanking {
+  [x: string]: any[];
+}
+
+interface ITrendInfo {
+  data: any[];
+  xAxis: string[];
+}
+export interface ITrend {
+  qualityPoints: ITrendInfo;
+  codeLines: ITrendInfo;
+  codeBugs: ITrendInfo;
+  utPassRate: ITrendInfo;
+  codeDuplicationsRate: ITrendInfo;
+  utCovRate: ITrendInfo;
+}
