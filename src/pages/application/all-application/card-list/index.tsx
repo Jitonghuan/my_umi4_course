bd78c3d09503fb03c9725e04527267eb85a89141
mingmei.wmm <wumingmei@come-future.com>
2021-11-17 20:16:46 +0800

    feat: 卡片优化

--- src/pages/application/all-application/card-list/index.tsx
+++ src/pages/application/all-application/card-list/index.tsx
@@ -48,11 +48,7 @@ export default function ApplicationCardList(props: IProps) {
             })
           }
         >
-          {item.appType === 'frontend' ? (
-            <div className={`${cardCls}-sub-header`}>{item.deploymentName}</div>
-          ) : (
-            <div className={`${cardCls}-header`}>{item.appName}</div>
-          )}
+          <div className={`${cardCls}-header`}>{item.deploymentName}</div>
 
           <div className={`${cardCls}-content`}>
             {/* <div>应用类型：{APP_TYPE_MAP[item.appType]}</div> */}
