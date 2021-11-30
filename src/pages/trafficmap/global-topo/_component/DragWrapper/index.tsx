e965504c3d03d63339b061af3f8428b0901a5b26
shixia.ds <daishu@come-future.com>
2021-11-30 17:06:33 +0800

    feat:DragWrapper改为函数组件

--- src/pages/trafficmap/global-topo/_component/DragWrapper/index.tsx
+++ src/pages/trafficmap/global-topo/_component/DragWrapper/index.tsx
@@ -3,18 +3,10 @@
  * @Date: 2021-11-30 16:13:42
  * @Description:
  */
-/*
- * @Author: shixia.ds
- * @Date: 2021-11-29 11:30:37
- * @Description:
- */
 import React from 'react';
-import { Button } from 'antd';
-import { CloseOutlined } from '@ant-design/icons';
-import Draggable from 'react-draggable';
 import LineChart from '../../../_component/line-chart';
 import './index.less';
-import { IAppInfo, IChartData } from '@/pages/trafficmap/interface';
+import { IAppInfo } from '@/pages/trafficmap/interface';
 import DragModal from '../DragModal';
 
 const lineChartTmp = [
@@ -32,52 +24,26 @@ const lineChartTmp = [
   },
 ];
 
-export default class DragWrapper extends React.Component<any, any> {
-  render() {
-    return (
-      <div className="drag-wrapper">
-        {this.props.appInfoList.map((appInfo: IAppInfo) => {
-          return (
-            <DragModal
-              title={appInfo.name}
-              onCancel={() => {
-                this.props.deleteModal(appInfo);
-              }}
-            >
-              <div className="line-chart-group">
-                {lineChartTmp.map((item, index: number) => (
-                  <LineChart {...item} {...appInfo.chartData[item.key]} key={index} />
-                ))}
-              </div>
-            </DragModal>
-            // <Draggable handle=".drag-header" {...dragHandlers} bounds="body">
-            //   <div className="app-modal" style={{ minWidth: '260px', maxWidth: '400px' }}>
-            //     <div className="app-modal-content no-cursor">
-            //       <Button
-            //         className="app-modal-close"
-            //         icon={<CloseOutlined />}
-            //         onClick={() => {
-            //           this.props.deleteModal(appInfo);
-            //         }}
-            //       />
-            //       {/* <strong className="cursor"> */}
-            //       <div className="app-modal-header drag-header cursor">
-            //         <div className="app-modal-title">{appInfo.name}</div>
-            //       </div>
-            //       {/* </strong> */}
-            //       <div className="app-modal-body">
-            //         <div className="line-chart-group">
-            //           {lineChartTmp.map((item, index: number) => (
-            //             <LineChart {...item} {...appInfo.chartData[item.key]} key={index} />
-            //           ))}
-            //         </div>
-            //       </div>
-            //     </div>
-            //   </div>
-            // </Draggable>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const DragWrapper: React.FC<any> = (props) => {
+  return (
+    <div className="drag-wrapper">
+      {props.appInfoList.map((appInfo: IAppInfo) => {
+        return (
+          <DragModal
+            title={appInfo.name}
+            onCancel={() => {
+              props.deleteModal(appInfo);
+            }}
+          >
+            <div className="line-chart-group">
+              {lineChartTmp.map((item, index: number) => (
+                <LineChart {...item} {...appInfo.chartData[item.key]} key={index} />
+              ))}
+            </div>
+          </DragModal>
+        );
+      })}
+    </div>
+  );
+};
+export default DragWrapper;
