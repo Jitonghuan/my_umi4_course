7ca03f4f92e31b22cf50d669e63b5695d1c5627b
冰果 <laiweilai@bingguodeMacBook-Pro.local>
2022-02-14 14:23:57 +0800

    冲突一键解决-按钮完成

--- src/pages/application/application-detail/components/application-deploy/deploy-content/components/merge-conflict/MonacoEditor.tsx
+++ src/pages/application/application-detail/components/application-deploy/deploy-content/components/merge-conflict/MonacoEditor.tsx
@@ -5,12 +5,46 @@ import './monaco.less';
 import 'monaco-editor/esm/vs/editor/contrib/find/findController.js';
 import ReactDOM from 'react-dom';
 import { strSplice } from '@/common/util';
+const temp: any = `import org.springframework.web.bind.annotation.Requ222estMapping;
+import org.springframework.web.bind.annotation.Re22stController;
+import org.springframework.web.bind.annotation.Re22stController;
+import org.springframework.web.bind.annotation.Re22stController;
+import org.springframework.web.bind.annotation.Re22stController;
+import org.springframework.web.bind.annotation.Re22stController;
 
+@RestController
+public class HealthCheckContr222oller {
+
+    @RequestMapping("/health")
+    public String heakthCheck() {
+    
+<<<<<<< HEAD
+        return "successfefef222";
+=======
+        return "success111";
+>>>>>>> feature_conflict1_20220127184906
+
+<<<<<<< HEAD
+        return "success333";
+=======
+        return "success444";
+>>>>>>> feature_conflict1_20220127184906
+    }
+
+<<<<<<< HEAD
+          return "success333";
+=======
+          return "success444";
+>>>>>>> feature_conflict1_20220127184906
+    }
+}`;
+const file = 'aa.jave';
 export default function MonacoEditor(prop: any) {
-  const { filePath, context, resolved, onchange } = prop;
+  const { filePath = file, context = temp, resolved, onchange } = prop;
   const [instance, setInstance] = useState<editor.IStandaloneCodeEditor | undefined>(undefined);
   const [decorations, setDecorations] = useState<string[]>([]);
   const [oldZones, setOldZones] = useState<string[]>([]);
+  const [tooltipWidgets, setTooltipWidgets] = useState<any[]>([]);
   const codeContainer = useCallback((node) => {
     if (node) {
       var editor = monaco.editor.create(node, {});
@@ -70,7 +104,8 @@ export default function MonacoEditor(prop: any) {
           if (char === '=') {
             if (continueWith('=======', index)) {
               current[mode] = index;
-              //   current.oldValue = str.substring(str.indexOf('\n', current.start), current.split);
+              current.oldValue = str.substring(str.indexOf('\n', current.start), current.split);
+              current.startEnd = str.indexOf('\n', current.start);
               mode = e;
               index += '======='.length;
             }
@@ -81,8 +116,8 @@ export default function MonacoEditor(prop: any) {
           if (char === '>') {
             if (continueWith('>>>>>>>', index)) {
               current[mode] = index;
-              //   current.newValue = str.substring(str.indexOf('\n', current.split), current.end);
-              //   current.repStop = str.indexOf('\n', current.end);
+              current.newValue = str.substring(str.indexOf('\n', current.split), current.end);
+              current.repStop = str.indexOf('\n', current.end);
               res.push(current);
               current = {};
               mode = s;
@@ -91,13 +126,56 @@ export default function MonacoEditor(prop: any) {
           }
       }
     } while (index++ < str.length);
-
+    console.log(res, 11);
     return res;
+  };
+  // head后以及尾部后面的提示文字
+  let renderTooltipWidget = (areas: any) => {
+    if (!instance) return;
+    tooltipWidgets.forEach((e) => instance.removeContentWidget(e));
+    const currentWidgets = areas.map((item: any) => {
+      var domNode = document.createElement('div');
+      domNode.className = 'merge-tooltip merge-editor-text';
+      ReactDOM.render(<>(Current Change)</>, domNode);
+      return createWidget(item.startEnd, domNode);
+    });
+    const inComingWidgets = areas.map((item: any) => {
+      var domNode = document.createElement('div');
+      domNode.className = 'merge-tooltip merge-editor-texts';
+      ReactDOM.render(<>(Incoming Change)</>, domNode);
+      return createWidget(item.repStop, domNode);
+    });
+    setTooltipWidgets(currentWidgets.concat(inComingWidgets));
+  };
+
+  let createWidget = (position: number, domNode: any) => {
+    if (!instance) return;
+    let id = Math.floor(Math.random() * (100000000 - 1) + 1);
+    var contentWidget: any = {
+      getId: function () {
+        return 'widget-id-' + id;
+      },
+      getDomNode: function () {
+        return domNode;
+      },
+      getPosition: function () {
+        return {
+          position: {
+            lineNumber: instance.getModel()?.getPositionAt(position).lineNumber || 1,
+            column: instance.getModel()?.getPositionAt(position).column || 1,
+          },
+          preference: [monaco.editor.ContentWidgetPositionPreference.EXACT],
+        };
+      },
+    };
+    instance.addContentWidget(contentWidget);
+    return createWidget;
   };
 
   let renderMergeTools = () => {
     if (!instance) return;
     const areas = diffAreas();
+    renderTooltipWidget(areas);
     // 冲突区域高亮
     const newDecorations = areas.map((position: any) => ({
       options: {
@@ -122,40 +200,42 @@ export default function MonacoEditor(prop: any) {
     }));
     // 保证有更新时清除旧的高亮 同时更新最新的高亮区域
     setDecorations(instance.deltaDecorations(decorations, newDecorations));
-
+    // 点击按钮时更新值
     function replaceValue(area: any, rep: string) {
       if (!instance) return;
-      //    https://stackoverflow.com/a/41667840
-      //   let ns = strSplice(instance.getModel()?.getValue() || '', area.start, area.repStop - area.start, rep);
-      //   instance.getModel()?.setValue(ns);
     }
-    // // 增加快速操作按钮
-    // instance.changeViewZones(function (changeAccessor) {
-    //   oldZones.forEach((e) => changeAccessor.removeZone(e));
-    //   setOldZones(
-    //     areas.map((area: any) => {
-    //       var domNode = document.createElement('div');
-    //       domNode.className = 'merge-ops-sence merge-ops';
-    //       ReactDOM.render(
-    //         <>
-    //           <button className="merge-item" onClick={() => replaceValue(area, area.oldValue)}>
-    //             使用当前分支
-    //           </button>
-    //           ｜
-    //           <button className="merge-item" onClick={() => replaceValue(area, area.newValue)}>
-    //             使用待合并分支
-    //           </button>
-    //         </>,
-    //         domNode,
-    //       );
-    //       return changeAccessor.addZone({
-    //         afterLineNumber: (instance.getModel()?.getPositionAt(area.start).lineNumber || 0) - 1,
-    //         heightInLines: 1,
-    //         domNode: domNode,
-    //       });
-    //     }),
-    //   );
-    // });
+
+    // 增加快速操作按钮
+    instance.changeViewZones(function (changeAccessor) {
+      oldZones.forEach((e) => changeAccessor.removeZone(e));
+      setOldZones(
+        areas.map((area: any) => {
+          var domNode = document.createElement('div');
+          domNode.className = 'merge-ops-sence merge-ops';
+          ReactDOM.render(
+            <>
+              <button className="merge-item" onClick={() => replaceValue(area, area.oldValue)}>
+                Accept Current Change
+              </button>
+              ｜
+              <button className="merge-item" onClick={() => replaceValue(area, area.newValue)}>
+                Accept Incoming Change
+              </button>
+              |
+              <button className="merge-item" onClick={() => replaceValue(area, area.newValue)}>
+                Accept Both Change
+              </button>
+            </>,
+            domNode,
+          );
+          return changeAccessor.addZone({
+            afterLineNumber: (instance.getModel()?.getPositionAt(area.start).lineNumber || 0) - 1,
+            heightInLines: 1,
+            domNode: domNode,
+          });
+        }),
+      );
+    });
   };
 
   useEffect(() => {
