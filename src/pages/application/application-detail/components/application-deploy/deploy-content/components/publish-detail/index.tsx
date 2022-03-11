341c71cb16d3071ad61f70ba446e8f7510712ae6
冰果 <laiweilai@bingguodeMacBook-Pro.local>
2022-03-11 19:28:21 +0800

    fix:修复报错

--- src/pages/application/application-detail/components/application-deploy/deploy-content/components/publish-detail/index.tsx
+++ src/pages/application/application-detail/components/application-deploy/deploy-content/components/publish-detail/index.tsx
@@ -228,7 +228,7 @@ export default function PublishDetail(props: IProps) {
         message.success(`${info.file.name} 上传成功`);
       } else if (info.file.status === 'error') {
         message.error(`${info.file.name} 上传失败`);
-      } else {
+      } else if (info.file?.response.success == 'false') {
         message.error(info.file.response.errorMsg || '');
       }
       setDeployVisible(false);
@@ -540,7 +540,7 @@ export default function PublishDetail(props: IProps) {
 
         <div style={{ display: 'flex', marginTop: '12px' }} key={Math.random()}>
           <span>配置文件：</span>
-          <Upload {...uploadProps} accept=".tgz">
+          <Upload {...uploadProps} accept=".tgz,.tar.gz">
             <Button icon={<UploadOutlined />} type="primary" ghost disabled={!deployEnv?.length}>
               离线部署
             </Button>
