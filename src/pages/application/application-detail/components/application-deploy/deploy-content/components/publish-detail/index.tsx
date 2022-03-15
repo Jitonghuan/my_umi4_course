8a163f90027750e1cc58ef817595f76cebb643e4
muxi.jth <2016670689@qq.com>
2022-03-15 10:30:19 +0800

    chore:save

--- src/pages/application/application-detail/components/application-deploy/deploy-content/components/publish-detail/index.tsx
+++ src/pages/application/application-detail/components/application-deploy/deploy-content/components/publish-detail/index.tsx
@@ -228,8 +228,8 @@ export default function PublishDetail(props: IProps) {
         message.success(`${info.file.name} 上传成功`);
       } else if (info.file.status === 'error') {
         message.error(`${info.file.name} 上传失败`);
-      } else if (info.file?.response.success == 'false') {
-        message.error(info.file.response.errorMsg || '');
+      } else if (info.file?.response.success === false) {
+        message.error(info.file.response.errorMsg);
       }
       setDeployVisible(false);
       setDeployEnv([]);
