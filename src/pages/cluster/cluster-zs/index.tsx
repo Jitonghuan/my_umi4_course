b4de7d456d42f43bdbb07e5866f12ce0d1438c8b
muxi.jth <2016670689@qq.com>
2022-03-24 00:55:21 +0800

    chore：去掉111

--- src/pages/cluster/cluster-zs/index.tsx
+++ src/pages/cluster/cluster-zs/index.tsx
@@ -20,12 +20,12 @@ export default function Dashboard(props: any) {
     <PageContainer>
       <FilterCard className="layout-compact">
         <Tabs activeKey={activeKey} onChange={(next) => props.history.push(`/matrix/cluster/cluster-zs/${next}`)}>
-          <Tabs.TabPane tab="集群看板111" key="dashboards" />
-          <Tabs.TabPane tab="流量调度111" key="scheduling-mode" />
+          <Tabs.TabPane tab="集群看板" key="dashboards" />
+          <Tabs.TabPane tab="流量调度" key="scheduling-mode" />
           {/* <Tabs.TabPane tab="集群调度" key="scheduling" /> */}
-          <Tabs.TabPane tab="集群同步111" key="cluster-sync" />
-          <Tabs.TabPane tab="应用同步111" key="application-sync" />
-          <Tabs.TabPane tab="操作记录11" key="operation-log" />
+          <Tabs.TabPane tab="集群同步" key="cluster-sync" />
+          <Tabs.TabPane tab="应用同步" key="application-sync" />
+          <Tabs.TabPane tab="操作记录" key="operation-log" />
         </Tabs>
       </FilterCard>
       <VCPermission code={window.location.pathname} isShowErrorPage>
