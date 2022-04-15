d97a3f3f34f8d46ba8ebab39fdc511dfde28fade
muxi.jth <2016670689@qq.com>
2022-04-15 17:44:42 +0800

    chore:save

--- src/pages/operation/env-manage/env-list/block-modal/index.tsx
+++ src/pages/operation/env-manage/env-list/block-modal/index.tsx
@@ -27,6 +27,7 @@ export default function NGModalDetail(props: NGInfo) {
   const [appsListData, setAppsListData] = useState<any>([]);
   const [targetKeys, setTargetKeys] = useState([]); //目标选择的key值
   const [selectedKeys, setSelectedKeys] = useState<any>([]); //已经选择的key值
+  const [disabled, setDisabled] = useState<boolean>(false);
   const onChange = (nextTargetKeys: any, direction: any, moveKeys: any) => {
     setTargetKeys(nextTargetKeys);
   };
@@ -49,6 +50,7 @@ export default function NGModalDetail(props: NGInfo) {
     return () => {
       setTargetKeys([]);
       setSelectedKeys(undefined);
+      setDisabled(false);
     };
   }, [visible]);
 
@@ -109,6 +111,10 @@ export default function NGModalDetail(props: NGInfo) {
             }); //从整体数据源中筛选，其中不包含可选数据中所有的key值，即为已选数据，则setState目标数据key数组中,视图渲染
             setTargetKeys(selectedAppCode);
           }
+        } else {
+          setAppsListData([]);
+          setDisabled(true);
+          return;
         }
       })
       .finally(() => {
@@ -172,14 +178,15 @@ export default function NGModalDetail(props: NGInfo) {
           关闭
         </Button>,
         <Popconfirm
-          title="确定要对这些应用进行封网吗？"
+          title="确定要对这些应用进行操作吗？"
           onConfirm={handleOk}
           okText="确定"
           cancelText="取消"
           placement="topRight"
+          disabled={disabled}
         >
-          <Button type="primary" loading={ensureLoading}>
-            确定封网
+          <Button type="primary" danger loading={ensureLoading}>
+            执行
           </Button>
         </Popconfirm>,
       ]}
@@ -187,7 +194,10 @@ export default function NGModalDetail(props: NGInfo) {
       <div className="block-data-info">
         <CheckCircleOutlined style={{ color: 'green' }} />
         <span style={{ marginLeft: 10, fontSize: 14 }}>
-          <b>当前环境：{initData.envName}</b>
+          <b>
+            当前环境：{initData.envName}
+            <span style={{ marginLeft: 6 }}>({initData.envCode})</span>
+          </b>
         </span>
       </div>
       <Spin spinning={loading}>
