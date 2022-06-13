85d4c7a32cc4f2690e69a415f4899966a7f79371
muxi.jth <2016670689@qq.com>
2022-06-13 16:36:24 +0800

    chore:save

--- src/pages/monitor/board/components/alarm-modal/index.tsx
+++ src/pages/monitor/board/components/alarm-modal/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { CheckCircleOutlined, RedoOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
 import { Form, Table, Modal } from 'antd';
 import { alarmTableSchema } from './schema';
+import { queryClusterAlertInfo } from '../../service';
 import './index.less';
 export interface boardInfo extends Record<string, any> {
   mode: string;
@@ -11,12 +11,25 @@ export interface boardInfo extends Record<string, any> {
 
 export function AlarmModal(props: boardInfo) {
   const { onClose, mode, curClusterId } = props;
+  const [dataSource, setDataSource] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     if (curClusterId) {
+      queryAlarmList();
     } else {
       return;
     }
   }, [mode]);
+  const queryAlarmList = () => {
+    setLoading(true);
+    queryClusterAlertInfo({ clusterId: curClusterId })
+      .then((res) => {
+        setDataSource(res);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   return (
     <Modal
@@ -34,7 +47,8 @@ export function AlarmModal(props: boardInfo) {
           expandedRowRender: (record: any) => <p style={{ margin: 0 }}>{record?.lables || '--'}</p>,
           rowExpandable: (record) => record.name !== 'Not Expandable',
         }}
-        dataSource={[]}
+        dataSource={dataSource}
+        loading={loading}
       ></Table>
     </Modal>
   );
