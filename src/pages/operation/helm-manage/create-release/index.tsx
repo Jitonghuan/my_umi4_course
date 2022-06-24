032709d956bc8d17e259e82ccae1bc962b6cffa5
muxi.jth <2016670689@qq.com>
2022-06-24 14:17:28 +0800

    chore：save

--- src/pages/operation/helm-manage/create-release/index.tsx
+++ src/pages/operation/helm-manage/create-release/index.tsx
@@ -9,24 +9,48 @@ export default function CreateRelease() {
   return (
     <PageContainer>
       <ContentCard>
-        <Form
-          labelCol={{ flex: '120px' }}
-          form={createReleaseForm}
-          onReset={() => {
-            createReleaseForm.resetFields();
-          }}
-        >
-          <Form.Item label="发布名称">
-            <Input style={{ width: 320 }} />
-          </Form.Item>
-          <Form.Item label="命名空间">
-            <Select style={{ width: 320 }} allowClear showSearch />
-          </Form.Item>
-          <Form.Item label="chart名称">
-            <Select style={{ width: 320 }} allowClear showSearch />
-          </Form.Item>
-        </Form>
-        <CreatCard />
+        <div className="create-card-content">
+          <Form
+            labelCol={{ flex: '120px' }}
+            form={createReleaseForm}
+            onReset={() => {
+              createReleaseForm.resetFields();
+            }}
+          >
+            <Form.Item label="发布名称">
+              <Input style={{ width: 320 }} />
+            </Form.Item>
+            <Form.Item label="命名空间">
+              <Select style={{ width: 320 }} allowClear showSearch />
+            </Form.Item>
+            <Form.Item label="chart名称">
+              <Select style={{ width: 320 }} allowClear showSearch />
+            </Form.Item>
+          </Form>
+
+          {/* <div className={`${rootCls}__card-wrapper`}>
+            {!isLoading && !appListData.length && (
+              <Empty style={{ paddingTop: 100 }} image={Empty.PRESENTED_IMAGE_SIMPLE} />
+            )}
+            <CreatCard key={type} type={type} dataSource={appListData} loadAppListData={loadAppListData} />
+            {total > 10 && (
+              <div className={`${rootCls}-pagination-wrap`}>
+                <Pagination
+                  pageSize={pageSize}
+                  total={total}
+                  current={pageIndex}
+                  showSizeChanger
+                  onShowSizeChange={(_, next) => {
+                    setPageIndex(1);
+                    setPageSize(next);
+                  }}
+                  onChange={(next) => setPageIndex(next)}
+                />
+              </div>
+            )}
+          </div> */}
+          <CreatCard />
+        </div>
       </ContentCard>
     </PageContainer>
   );
