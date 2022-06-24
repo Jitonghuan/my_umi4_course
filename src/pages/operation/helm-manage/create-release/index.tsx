775d7298d2c171846582d387a5c03a563578bac6
muxi.jth <2016670689@qq.com>
2022-06-24 15:58:30 +0800

    chore：save

--- src/pages/operation/helm-manage/create-release/index.tsx
+++ src/pages/operation/helm-manage/create-release/index.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Tag, Tooltip, Popconfirm, Form, Input, Select } from 'antd';
+import { Tag, Tooltip, Popconfirm, Form, Input, Select, Button, Space } from 'antd';
 import PageContainer from '@/components/page-container';
 import CreatCard from '../create-release/components/create-card';
 import { ContentCard } from '@/components/vc-page-content';
+import './index.less';
 
 export default function CreateRelease() {
   const [createReleaseForm] = Form.useForm();
+  const [showNextStep, setShowNextStep] = useState<boolean>(false);
   return (
     <PageContainer>
       <ContentCard>
-        <div className="create-card-content">
+        <div className="create-release-content">
           <Form
             labelCol={{ flex: '120px' }}
             form={createReleaseForm}
@@ -17,15 +19,30 @@ export default function CreateRelease() {
               createReleaseForm.resetFields();
             }}
           >
-            <Form.Item label="发布名称">
-              <Input style={{ width: 320 }} />
-            </Form.Item>
-            <Form.Item label="命名空间">
-              <Select style={{ width: 320 }} allowClear showSearch />
-            </Form.Item>
-            <Form.Item label="chart名称">
-              <Select style={{ width: 320 }} allowClear showSearch />
-            </Form.Item>
+            {!showNextStep && (
+              <>
+                <Form.Item label="发布名称">
+                  <Input style={{ width: 320 }} />
+                </Form.Item>
+                <Form.Item label="命名空间">
+                  <Select style={{ width: 320 }} allowClear showSearch />
+                </Form.Item>
+                <Form.Item label="chart名称">
+                  <Select style={{ width: 320 }} allowClear showSearch />
+                </Form.Item>
+              </>
+            )}
+
+            {showNextStep && (
+              <>
+                <Form.Item label="chart版本">
+                  <Select style={{ width: 320 }} allowClear showSearch />
+                </Form.Item>
+                <Form.Item>
+                  <Input.TextArea style={{ width: 320 }} />
+                </Form.Item>
+              </>
+            )}
           </Form>
 
           {/* <div className={`${rootCls}__card-wrapper`}>
@@ -49,7 +66,36 @@ export default function CreateRelease() {
               </div>
             )}
           </div> */}
-          <CreatCard />
+          <div className="create-card-content">
+            <CreatCard />
+          </div>
+          <div className="create-card-footer">
+            {!showNextStep && (
+              <Space>
+                <Button
+                  onClick={() => {
+                    setShowNextStep(true);
+                  }}
+                >
+                  下一步
+                </Button>
+                <Button>取消</Button>
+              </Space>
+            )}
+            {showNextStep && (
+              <Space>
+                <Button
+                  onClick={() => {
+                    setShowNextStep(false);
+                  }}
+                >
+                  上一步
+                </Button>
+                <Button>确定</Button>
+                <Button>取消</Button>
+              </Space>
+            )}
+          </div>
         </div>
       </ContentCard>
     </PageContainer>
