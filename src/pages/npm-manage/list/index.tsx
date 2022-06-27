a159f40bf261404725e1fbb00fbaa273bf9f0a3a
mingmei.wmm <wumingmei@come-future.com>
2022-06-27 15:04:25 +0800

    feat: npm

--- src/pages/npm-manage/list/index.tsx
+++ src/pages/npm-manage/list/index.tsx
@@ -1,14 +1,26 @@
-import React, { useMemo, useState, useCallback, useContext } from 'react';
-import {Button, Form, Input, message, Select, Table} from 'antd';
+import React, { useState, useEffect } from 'react';
+import {Button, Form, Input, Table} from 'antd';
 import PageContainer from '@/components/page-container';
-import { FilterCard } from '@/components/vc-page-content';
+import { FilterCard, ContentCard } from '@/components/vc-page-content';
+import { PlusOutlined } from '@ant-design/icons';
+import { history } from 'umi';
 import './index.less';
+
+
 const { Item: FormItem } = Form;
 
 export default function NpmList() {
   const [searchField] = Form.useForm();
+  const [dataList, setDataList] = useState([]);
+  const [pageSize, setPageSize] = useState(10);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
 
-  function handleSearch() {}
+  function handleSearch(pagination?: any) {}
+
+  useEffect(() => {
+    handleSearch();
+  }, [])
 
   return (
     <PageContainer className="npm-list-page">
@@ -32,8 +44,72 @@ export default function NpmList() {
               重置
             </Button>
           </FormItem>
+          <FormItem noStyle>
+            <div className="list-btn-wrapper">
+              <Button type="primary" icon={<PlusOutlined />}>新增</Button>
+            </div>
+          </FormItem>
         </Form>
       </FilterCard>
+      <ContentCard>
+        <Table
+          bordered
+          dataSource={dataList}
+          rowKey='name'
+          scroll={{ x: '100%' }}
+          pagination={{
+            total,
+            pageSize,
+            current: page,
+            onChange: (page, pageSize) => {
+              setPage(page);
+              setPageSize(pageSize);
+              handleSearch({
+                page,
+                pageSize
+              })
+            }
+          }}
+          columns={[
+            {
+              title: '包名',
+              dataIndex: 'name'
+            },
+            {
+              title: '描述',
+              dataIndex: 'desc'
+            },
+            {
+              title: '负责人',
+              dataIndex: 'owner'
+            },
+            {
+              width: 140,
+              title: '操作',
+              fixed: 'right',
+              dataIndex: 'operate',
+              align: 'center',
+              render: (_: any, record: any, index: number) => (
+                <div className="action-cell">
+                  <a
+                    onClick={() => {
+                      history.push({
+                        pathname: 'detail',
+                        query: {
+                          id: record.id,
+                          appCode: record.appCode,
+                        },
+                      });
+                    }}
+                  >
+                    详情
+                  </a>
+                </div>
+              ),
+            }
+          ]}
+          />
+      </ContentCard>
     </PageContainer>
   );
 }
