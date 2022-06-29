f0beb84666b39436220b463b2df3c7da4c3d8bba
muxi.jth <2016670689@qq.com>
2022-06-29 11:40:22 +0800

    chore:save

--- src/app.config.ts
+++ src/app.config.ts
@@ -41,6 +41,7 @@ const PRIVATE_METHODS = getPrivateMethods(false);
 const IS_Matrix = getPrivateMethods(true);
 
 let envType = BUILD_ENV === 'prod' ? 'prod' : 'dev';
+envType = window.location.href.includes('matrix-jyglj') ? 'jyglj' : envType;
 envType = window.location.href.includes('matrix-bf-daily') ? 'bf-daily' : envType;
 envType = window.location.href.includes('matrix-fygs') ? 'fygs' : envType;
 envType = window.location.href.includes('matrix-zslnyy') ? 'zslnyy' : envType;
