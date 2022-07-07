1b834054c0eb9dff90b75cd955e09bda0f513360
muxi.jth <2016670689@qq.com>
2022-07-07 16:25:44 +0800

    chore：save

--- src/pages/operation/helm-manage/helm-detail/components/history-log/index.tsx
+++ src/pages/operation/helm-manage/helm-detail/components/history-log/index.tsx
@@ -63,23 +63,23 @@ export default function HistoryLog(props: PorpsItem) {
     {
       title: '发布名称',
       dataIndex: 'releaseName',
-      width: '20%',
+      width: '25%',
     },
     {
       title: '命名空间',
       dataIndex: 'namespace',
-      width: '26%',
+      width: '25%',
     },
     {
       title: '更新时间',
       dataIndex: 'updated',
-      width: '22%',
+      width: '25%',
       render: (value: string) => datetimeCellRender(value),
     },
     {
       title: ' 版本',
       dataIndex: 'revision',
-      width: '20%',
+      width: '12%',
     },
 
     {
