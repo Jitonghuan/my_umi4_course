0a63b41d1e4e3e4d96fbe946495b8b542429938b
muxi.jth <2016670689@qq.com>
2022-11-15 18:30:25 +0800

    chore:save

--- src/pages/monitor/application/schema.ts
+++ src/pages/monitor/application/schema.ts
@@ -226,6 +226,7 @@ export const getMemoryChartOption: any = (xAxis = [], dataSource = []) => {
     });
     nameArry.push(item?.name);
   });
+  
   return {
     tooltip: {
       trigger: 'axis',
