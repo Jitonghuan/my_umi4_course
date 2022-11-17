d8a87339bb1799ad10641ee1f99818c97858179c
muxi.jth <2016670689@qq.com>
2022-11-17 17:27:54 +0800

    chore:save

--- src/pages/monitor/network-dail/edit-dail/index.tsx
+++ src/pages/monitor/network-dail/edit-dail/index.tsx
@@ -1,34 +1,35 @@
-import { Collapse,Drawer } from 'antd';
+import { Collapse, Drawer } from 'antd';
 import React from 'react';
 
 const { Panel } = Collapse;
 
 const text = (
-  <p style={{ paddingLeft: 24 }}>
-    A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
-    as a welcome guest in many households across the world.
-  </p>
+    <p style={{ paddingLeft: 24 }}>
+        A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found
+        as a welcome guest in many households across the world.
+    </p>
 );
-interface Iprops{
-    mode:EditorMode
+interface Iprops {
+    mode: EditorMode
 }
-export default function EditDail (props:Iprops) {
-    const { mode,  } = props;
-    
-    return(
-        <Drawer  visible={mode !== 'HIDE'}>
+export default function EditDail(props: Iprops) {
+    const { mode, } = props;
+
+    return (
+        <Drawer title={mode === "ADD" ? "拨测新增" : "拨测编辑"} visible={mode !== 'HIDE'} width={"80%"}>
             <Collapse bordered={false} defaultActiveKey={['1']}>
-    <Panel header="网络拨测编辑" key="1">
-      {text}
-    </Panel>
-    <Panel header="报警监控" key="2">
-      {text}
-    </Panel>
-   
-  </Collapse>
+                <Panel header={<h3>网络拨测编辑</h3>} key="1">
+                   
+                </Panel>
+                <Panel header={<h3>报警监控</h3>} key="2">
+                   
+                </Panel>
+
+            </Collapse>
 
         </Drawer>
-  
-);}
+
+    );
+}
 
 //export default EditDail;
