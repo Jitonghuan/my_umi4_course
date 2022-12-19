d7ed234f51584d2468611550789ea2da052266bc
冰果 <bingguo@bingguos-MacBook-Pro.local>
2022-12-19 14:04:37 +0800

    save

--- src/pages/application/application-detail/components/application-deploy/version-publish/component/version-record/type.ts
+++ src/pages/application/application-detail/components/application-deploy/version-publish/component/version-record/type.ts
@@ -1,5 +1,6 @@
 
 export const statusMap: any = {
     process: { label: '正在打包', color: 'lime' },
-    finish: { label: '发版完成', color: "geekblue" }
+    finish: { label: '发版完成', color: "geekblue" },
+    cancel: { label: '发布未完成', color: "cyan" },
 }
