9b1b3109b70c89374419e7e9e99a7a46ba2c7cbc
muxi.jth <2016670689@qq.com>
2022-12-28 11:50:51 +0800

    chore:save

--- src/pages/database/instance-list/components/instance-info/components/logger/index.tsx
+++ src/pages/database/instance-list/components/instance-info/components/logger/index.tsx
@@ -2,26 +2,29 @@ import React, { useMemo, useState,} from 'react';
 import {Tabs } from 'antd';
 import PageContainer from '@/components/page-container';
 import { ContentCard } from '@/components/vc-page-content';
-// import SessionManage from './manage';
-// import LockPage from './lock'
+import LogConfig from './log-config';
+import LowSqlDetail from './lowSql-detail';
+import LowSqlStatistics from './lowSql-statistics';
 export default function SessionDiag(){
     const { TabPane } = Tabs;
     const [activeKey,setActiveKey]=useState<string>("manage")
-    // const filteredTabList = useMemo(() => {
-    //   return[
-    //     {key:"manage",tab:"会话管理",component:<SessionManage/>},
-    //     {key:"lock",tab:"锁分析",component:<LockPage/>}
-    //   ]
-    // },[])
+    const filteredTabList = useMemo(() => {
+      return[
+        {key:"statistics",tab:"慢日志统计",component:<LowSqlStatistics/>},
+        {key:"detail",tab:"慢日志明细",component:<LowSqlDetail/>},
+        {key:"config",tab:"日志配置",component: <LogConfig/>},
+       
+      ]
+    },[])
     return (
       <PageContainer>
            <ContentCard>
         <Tabs activeKey={activeKey} onChange={(key)=>{setActiveKey(key)}} >
-           {/* {filteredTabList.map((element) => (
+           {filteredTabList.map((element) => (
             <TabPane tab={element.tab} key={element.key}>
               {element?.component}
             </TabPane>
-          ))}   */}
+          ))}  
         </Tabs>  
         </ContentCard>
       </PageContainer>
