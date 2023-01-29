b5e8a791e5c5431d7069a0866e04ab2fba6696bf
muxi.jth <2016670689@qq.com>
2023-01-29 15:50:55 +0800

    chore:模版暂存

--- src/pages/application/application-detail/components/pipeline-tmpl/index.tsx
+++ src/pages/application/application-detail/components/pipeline-tmpl/index.tsx
@@ -1,32 +1,53 @@
-export default function PipeLineTmpl(){
-    const a={
-        b:1,
-        c:2
-    }
-    let c:string="b"
-    // 元素隐式具有 "any" 类型，因为类型为 "string" 的表达式不能用于索引类型 "{ b: number; c: number; }"。
-// 在类型 "{ b: number; c: number; }" 上找不到具有类型为 "string" 的参数的索引签名。
 
-    //const d =a[c];
-    const aq:{[key:string]:number}={
-        b:1,
-        c:2
+import React, { useContext, useRef } from 'react';
+import { Button, Row, Col, Form, Select, Space, message, Spin, Modal, Radio, Popconfirm } from 'antd';
+import { ContentCard } from '@/components/vc-page-content';
+import { getRequest, putRequest } from '@/utils/request';
+import { useState, useEffect } from 'react';
+import {  useLocation } from 'umi';
+import { parse } from 'query-string';
+import AceEditor from '@/components/ace-editor';
+import DetailContext from '@/pages/application/application-detail/context';
+import EditorTable from '@cffe/pc-editor-table';
+import * as APIS from '@/pages/application/service';
+import {getCicdTemplateList} from './service'
+
+
+const PipeLineTmpl=()=>{
+    const { appData } = useContext(DetailContext);
+    //getCicdTemplateList
+    const [tmplList,setTmplList]=useState<any>({})
+    const [flowType,setFlowType]=useState<any>([])
+    const getCicdTemplList=()=>{
+        setTmplList({})
+        getRequest(getCicdTemplateList,{data:{appCode:appData?.appCode}}).then((result)=>{
+            if(result?.success){
+                setFlowType(result?.data)
+
+            }
+
+        })
     }
-    interface A{
-        b:number
-        c:number
-    }
-    const ab:A={
-        b:1,
-        c:2
-    }
-    const ac:A & {[key:string]:number}={
-        b:1,
-        c:2
-    }
+    useEffect(()=>{
+        if(!appData?.appCode) return
+        getCicdTemplList()
+    },[])
     return(
-        <div>
+        <ContentCard>
+            <Form layout="horizontal">
+                <Form.Item label="发布类型">
+                    <Select  style={{width:260}} options={[]}/>
+                </Form.Item>
+                <Form.Item label="模版详情">
+                    <AceEditor height={window.innerHeight - 290}  mode="yaml" />
+                </Form.Item>
+                <div style={{display:"flex",justifyContent:"flex-end"}}>
+                    <Button type="primary">提交</Button>
+                </div>
+            </Form>
+        </ContentCard>
 
-        </div>
-    )
+        )
 }
+export default PipeLineTmpl;
+    
---
+++ src/pages/application/application-detail/components/pipeline-tmpl/service.ts
@@ -0,0 +1,9 @@
+//流水线管理接口
+//接口文档地址：https://come-future.yuque.com/sekh46/bbgc7f/dikd9ayr1edqxoet#mk9PB
+//2023/01/29 15:17
+import { addAPIPrefix } from '@/utils';
+///cicdTemplateObj/list
+
+/* GET 1、 查询cicd模版  */
+
+export const getCicdTemplateList = addAPIPrefix('/appManage/cicdTemplateObj/list');
