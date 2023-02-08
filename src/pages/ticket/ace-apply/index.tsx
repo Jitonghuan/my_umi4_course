43a990d3124c43862c17fee193d763ec608278ef
muxi.jth <2016670689@qq.com>
2023-02-08 21:30:19 +0800

    chore:save

--- src/pages/ticket/ace-apply/index.tsx
+++ src/pages/ticket/ace-apply/index.tsx
@@ -1,5 +1,8 @@
 import React, { useRef} from 'react';
 import PageContainer from '@/components/page-container';
+import { UserManage } from '@cffe/fe-zero-code-solution';
+import modulesMap from '@cffe/hmos-zero-code-material';
+// import React from 'react';
 import './index.less'
 const AceTicketApply=()=>{
     const frameRef = useRef<any>();
@@ -7,21 +10,25 @@ const AceTicketApply=()=>{
       };
     
     return(
-        <PageContainer>
+        // <PageContainer>
+        <div className="fe-hmos-nocode-user">
+        <UserManage modulesMap={modulesMap} />
+      </div>
+
           
-            <iframe 
-             ref={frameRef}
-             className="ticket-apply-iframe"
-             id="ticket-iframe"
-             name="ticket-iframe-detail"
-             src={"http://c2f.ace.cfuture.shop/user/:10001004?appCode=OPS&pageCode=WorkOrderList&modelCode=WorkOrder&menuId=46701"} 
-             frameBorder="0"
-            //  onLoad={hideSlideMenu}
-             ></iframe>
+            // <iframe 
+            //  ref={frameRef}
+            //  className="ticket-apply-iframe"
+            //  id="ticket-iframe"
+            //  name="ticket-iframe-detail"
+            //  src={"http://c2f.ace.cfuture.shop/user/:10001004?appCode=OPS&pageCode=WorkOrderList&modelCode=WorkOrder&menuId=46701"} 
+            //  frameBorder="0"
+            // //  onLoad={hideSlideMenu}
+            //  ></iframe>
 
           
 
-        </PageContainer>
+        // </PageContainer>
     )
 
 }
