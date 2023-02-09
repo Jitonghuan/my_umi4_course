0b1ec61a20fec8ab87dd5cbab414da76dcd746b3
muxi.jth <jitonghuan@come-future.com>
2023-02-09 18:03:41 +0800

    chore：save

---
+++ src/pages/algorithm/shuzu/shuzu.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: Jitonghuan 2016670689@qq.com
+ * @Date: 2023-02-09 17:07:35
+ * @LastEditors: Jitonghuan 2016670689@qq.com
+ * @LastEditTime: 2023-02-09 17:07:36
+ * @FilePath: /my_umi4_course/src/pages/algorithm/shuzu.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+const Arry=()=>{
+    
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+export default Arry;
